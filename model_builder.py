3a61de4184238e55d054de096ceb8e416aaeae53
Josh Haimson <jdhaimson@gmail.com>
2015-12-04 13:29:23 -0500

    debugged and better commenting

--- model_builder.py
+++ model_builder.py
@@ -10,20 +10,60 @@ from baseline_transformer import GetConcatenatedNotesTransformer, GetLatestNotes
 from icd_transformer import ICD9_Transformer
 from value_extractor_transformer import EFTransformer, LBBBTransformer, SinusRhythmTransformer, QRSTransformer, NYHATransformer, NICMTransformer
 
+
+#This should make adding transformers easier. You could add a transformer like
+# features_add = [baseline_features[x] for x in ['Labs_Latest_High', 'Car_tfidf']
+
+control_features = {   'all_ef' :  ('all_ef', EFTransformer, {'method' : 'all', 'num_horizon' : 1}),
+                        'mean_ef' : ('mean_ef', EFTransformer, {'method' : 'mean', 'num_horizon' : 5}),
+                        'max_ef' :  ('max_ef', EFTransformer, {'method' : 'max', 'num_horizon' : 5}),
+                        'lbbb':     ('lbbb', LBBBTransformer, {'time_horizon' : 30*3}),
+                        'sr':       ('sr', SinusRhythmTransformer, {'time_horizon' : 30*3}),
+                        'nyha':     ('nyha', NYHATransformer, {'time_horizon' : 30*3}),
+                        'nicm':     ('nicm', NICMTransformer, {'time_horizon' : 30*3}),
+                        'all_qrs':  ('all_qrs', QRSTransformer, {'method' : 'all', 'num_horizon' : 1}),
+                        'icd9':     ('icd9', ICD9_Transformer, {}),
+                        'car_tfidf':('car_tfidf', FeaturePipeline, [('notes_car', GetConcatenatedNotesTransformer, {'note_type' : 'Car'}),
+                                                                    ('tfidf_car', TfidfTransformer, {})]),    
+                        'lno_tfidf':('lno_tfidf', FeaturePipeline, [('notes_lno', GetConcatenatedNotesTransformer, {'note_type' : 'Lno'}),
+                                                                    ('tfidf_lno', TfidfTransformer, {})]),
+                        'enc':      ('enc', GetEncountersFeaturesTransformer, {'max_encounters' : 5}),
+                        'lab_all' : ('lab_all', FeaturePipeline, [('lab_to_dict', GetLabsCountsDictTransformer, {}), ('dict_to_vect', DictVectorizer, {})]),                         
+                        'lab_low' : ('lab_low', FeaturePipeline, [('lab_to_dict', GetLabsLowCountsDictTransformer, {}), ('dict_to_vect', DictVectorizer, {})]),                         
+                        'lab_high' : ('lab_high', FeaturePipeline, [('lab_to_dict', GetLabsHighCountsDictTransformer, {}), ('dict_to_vect', DictVectorizer, {})]),                         
+                        'lab_low_recent' : ('lab_low_recent', FeaturePipeline, [('lab_to_dict', GetLabsLatestLowDictTransformer, {}), ('dict_to_vect', DictVectorizer, {})]),                         
+                        'lab_high_recent' : ('lab_high_recent', FeaturePipeline, [('lab_to_dict', GetLabsLatestHighDictTransformer, {}), ('dict_to_vect', DictVectorizer, {})]),                         
+                        'lab_hist' : ('lab_hist', FeaturePipeline, [('lab_to_dict', GetLabsHistoryDictTransformer, {'time_thresholds_months' : [1]}), ('dict_to_vect', DictVectorizer, {})]),                         
+                     }  
+
+control_groups = { 'regex' : ['all_ef', 'mean_ef', 'max_ef', 'lbbb', 'sr', 'nyha', 'nicm', 'all_qrs'],
+                   'structured_only' : ['idc9', 'enc', 'lab_all', 'lab_low', 'lab_high', 'lab_low_recent', 'lab_high_recent', 'lab_hist'],
+                   'notes_tfidf' : ['car_tfidf', 'lno_tfidf'],
+                   'labs':  ['lab_all', 'lab_low', 'lab_high', 'lab_low_recent', 'lab_high_recent', 'lab_hist']
+                 }
+
+#These are empty, but might be useful
+adaboost_baseline = {  'method' : 'adaboost', 'model_args' : {'n_estimators' : 500}, 'features' : {} } 
+lr_baseline = {  'method' : 'lr', 'model_args' : {'C' : 1}, 'features' : {} } 
+
+
 regex_baseline = {  'method' : 'adaboost',
                     'model_args' : {'n_estimators' : 500},
                     'features' : {  'all_ef' :  (EFTransformer, {'method' : 'all', 'num_horizon' : 1}),
                                     'mean_ef' : (EFTransformer, {'method' : 'mean', 'num_horizon' : 5}),
                                     'max_ef' :  (EFTransformer, {'method' : 'max', 'num_horizon' : 5}),
-                                    'LBBB':     (LBBBTransformer, {'time_horizon' : 30*3}),
-                                    'SR':       (SinusRhythmTransformer, {'time_horizon' : 30*3}),
-                                    'NYHA':     (NYHATransformer, {'time_horizon' : 30*3}),
-                                    'NICM':     (NICMTransformer, {'time_horizon' : 30*3}),
+                                    'lbbb':     (LBBBTransformer, {'time_horizon' : 30*3}),
+                                    'sr':       (SinusRhythmTransformer, {'time_horizon' : 30*3}),
+                                    'nyha':     (NYHATransformer, {'time_horizon' : 30*3}),
+                                    'nicm':     (NICMTransformer, {'time_horizon' : 30*3}),
                                     'all_qrs':  (QRSTransformer, {'method' : 'all', 'num_horizon' : 1})
                                  }
                  }
 
+
+
 ##################
+# control : this is the pre-loaded set of method, model_args, and features. If any of the below are none, will default to these
 #
 # method : this is a string that is interpreted as a switch statement below
 # model_args : the **kwargs dict that are passed to your model, e.g. AdaBoostClassifier(**model_args)    
@@ -33,22 +73,30 @@ regex_baseline = {  'method' : 'adaboost',
 #           the transformer args are a list of (name, class, args) triples. For example:
 #           'Car' : (FeaturePipeline, [('car_notes', CarNotesTransformer, {'horizon' :3}), ('tfidf', TfidfTransformer, {})])
 #
-#           Besides making features, you can also change them. I thought this was an inutitive option. The simplest change
+#   ADD     For features_add, you must give a triple (name, class, args). You can write this yourself, or reference the lists above
+#               features_add = control_groups['labs'] + [control_features[x] for x in ['icd9', 'lbbb']]
+#
+#   CHANGE  Besides making features, you can also change them. I thought this was an inutitive option. The simplest change
 #           just calls the transformer by name and assigns a new value to one of the args. For example:
 #               features_change = {'ef_all' : {'num_horizon' : 10, 'time_horizon' : 30*4}}
 #           But you can also change thing inside feature pipelines. For example, to change the feature above:
 #               features_change = {['Car', 0] : {'horizon' : 10}}
-#
+#   
+#   REMOVE  Lastly, you can remove features. This is easy. No error will be thrown if you remove a feature not included.
+#               features_remove = ['icd9', 'max_ef', 'lab_high']
+#               features_remove = control_groups['regex'] + control_groups['labs']
 ################
 
 def build_model(control, method = None, model_args = None, features = None, features_add = None, features_change = None, features_remove = None):
 
+    #Modify method
     if method == None:
         method = control['method']
         method_diff = False
     else: 
         method_diff = not method == control['method']
 
+    #Modify model args
     if model_args == None and method_diff:
         model_args = dict()
         args_diff = True
@@ -58,28 +106,33 @@ def build_model(control, method = None, model_args = None, features = None, feat
     else:
         args_diff = True
 
+    #Modify the model features
     #keep in mind that features are a dictionary of tuples, e.g. 'name': (transformer_class, transformer_args)
     feature_diff = {'+': [], '-' : []}
     if features == None:
         features = control['features']
+        #add features
+        if features_add != None:
+            for add in features_add:
+                features[add[0]] = (add[1], add[2])
+            feature_diff['+'] += features_add
+        #remove features
         if features_remove != None:
             feature_diff['-'] += features_remove
             for remove in features_remove:
                 if remove in features:
                     features.pop(remove)
+        #change features
         if features_change != None:
             for change in features_change:#not robust to handle changing FeaturePipeline
                 if change[0] in features:
                     transformer, args = features[change[0]]
                     feature_diff['-'] += [(change[0], args)]
-                    args = change_args(transformer, args, change[1])
+                    args = change_args(transformer, args, change[1]) #recursive change call
                     features[change[0]] = (transformer, args)
                     feature_diff['+'] += [(change[0], args)]
-        if features_add != None:
-            for add in features_add:
-                features[add[0]] = (add[1], add[2])
-            feature_diff['+'] += features_add
 
+    #select classifier and build pipeline
     if method in ['clinical', 'decision', 'cdm', 'clinical decision model']:
         is_regression = False
         clf = ClinicalDecisionModel()
@@ -97,9 +150,12 @@ def build_model(control, method = None, model_args = None, features = None, feat
         else:
             raise ValueError("'" + method + "' is not a supported classification method")
 
+        #build the transformers
         transformer_list = []
         for feature_name in features:
-            transformer_list += [build_transformer(feature_name, features[feature_name])]
+            transformer_list += [build_transformer(feature_name, features[feature_name])] #recursive build call
+
+        #assemble pipeline
         model =  Pipeline([
                 ('feature_union', FeatureUnion(transformer_list)),
                 ('Classifier', clf)
--- queue_test.py
+++ queue_test.py
@@ -10,10 +10,12 @@ import itertools
 #   python -m 'This is a test' -n 500 -cv 3 -d 'from model_builder import build_model, regex_baseline' -r "build_model(regex_baseline, method = 'SVM', features_remove = ['NYHA'], features_add = {'min_ef' : (EFTransformer, {'method' = 'min', 'num_horizon' = 2})})"
 #
 #  Keys    Description                                 Default                    
+#   -h      displays help message                       
 #   -mes    message/description                         ""
 #   -n      number of patients                          25
 #   -cv     number of CV splits                         3
-#   -d      dependency string to be executed            "from model_builder import build_model, regex_baseline"
+#   -d      dependency string to be executed            "" (this dependency is only added on to default)
+#   -con    control                                     regex_baseline
 #   -met    method to override control                  adaboost
 #   -arg*   arguments to override control model params  None
 #   -a*     features to add                             None
@@ -32,13 +34,15 @@ import itertools
 
 queue_file_path =  "../experiments/experiments.csv"
 
+base_dependency = "from model_builder import build_model, regex_baseline, control_features, control_groups, adaboost_baseline, lr_baseline" 
+
 def queue_test(message = "", num_patients = 25, cv_splits = 3, build_model_string = "build_model(regex_baseline)", dependencies = "from model_builder import build_model, regex_baseline"):
     with open(queue_file_path, 'r+b') as queue_file:
         test_id = sum(1 for line in queue_file)
         writer = csv.writer(queue_file)
         writer.writerow([test_id, message, cv_splits, num_patients, dependencies, build_model_string, '0'])
 
-def queue_grid_search(grid = None, message = "", num_patients = 25, cv_splits = 3, dependencies = "from model_builder import build_model, regex_baseline", control_string = 'regex_baseline', method_string = 'None', model_args = 'None', features_add_string = 'None', features_change_string = 'None', features_remove_string = 'None'):
+def queue_grid_search(grid = None, message = "", num_patients = 25, cv_splits = 3, dependencies = "", control_string = 'regex_baseline', method_string = 'None', model_args = 'None', features_add_string = 'None', features_change_string = 'None', features_remove_string = 'None'):
 
     if not grid == None: 
         args_options = build_options(grid, model_args)
@@ -58,13 +62,13 @@ def queue_grid_search(grid = None, message = "", num_patients = 25, cv_splits = 
                     results += [make_model_string(control_string, method_string, args, add, change, remove)] 
 
     for model_string in results:
-        queue_test(message, num_patients, cv_splits, model_string, dependencies)
+        queue_test(message, num_patients, cv_splits, model_string, base_dependency  + "; " + dependencies)
 
 def make_model_string(control, method, args, add, change, remove):
     result = "build_model("
     result += "control = " + control + ", "
     if not method == 'None':
-        result += "method = " + method + ", "
+        result += "method = '" + method + "', "
     if not args == 'None':
         result += "model_args = " + args + ", "
     if not add == 'None':
@@ -98,23 +102,44 @@ def build_options(mapping, string):
 
 def main():
     inputs = sys.argv[1:]
-    if len(inputs) % 2 == 1:
-        raise ValueError("Uninterpretable input: " + str(inputs))
+    if len(inputs) == 1 and inputs[0] in ['-h', 'help']:
+       show_help()
+    else:  
+        if len(inputs) % 2 == 1:
+            raise ValueError("Uninterpretable input: " + str(inputs))
 
-    queue_args = dict()
-    args_converter = {"-con" : "control_string", "-a" : "features_add_string", "-met" : 'method_string', '-arg' : 'model_args', '-c' : 'features_change_string', "-g" : "grid", "-mes" : "message", "-n" : "num_patients", "-r" : "features_remove_string", "-cv" : "cv_splits", "-d" : "dependencies"}
+        queue_args = dict()
+        args_converter = {"-con" : "control_string", "-a" : "features_add_string", "-met" : 'method_string', '-arg' : 'model_args', '-c' : 'features_change_string', "-g" : "grid", "-mes" : "message", "-n" : "num_patients", "-r" : "features_remove_string", "-cv" : "cv_splits", "-d" : "dependencies"}
 
-    for i in range(len(inputs) / 2):
-        key = inputs[2*i]
-        value = inputs[2*i + 1]
-        if key in args_converter:
-            if key in ['-g', '-n', '-cv' ]:
-                value = eval(value)
-            queue_args[args_converter[key]] = value
-        else:
-            raise ValueError("Unkown key: " + key)       
+        for i in range(len(inputs) / 2):
+            key = inputs[2*i]
+            value = inputs[2*i + 1]
+            if key in args_converter:
+                if key in ['-g', '-n', '-cv' ]:
+                    value = eval(value)
+                queue_args[args_converter[key]] = value
+            else:
+                raise ValueError("Unkown key: " + key)       
 
-    queue_grid_search(**queue_args)
+        queue_grid_search(**queue_args)
+def show_help():
+
+    print "\033[95m   Keys    Description                                 Default\033[0m"                    
+    print '   -mes    message/description                         ""'
+    print '   -n      number of patients                          25'
+    print '   -cv     number of CV splits                         3'
+    print '   -d      dependency string to be executed            "" (this dependency is only added on to default)'
+    print '   -con    control                                     regex_baseline'
+    print '   -met    method to override control                  adaboost'
+    print '   -arg*   arguments to override control model params  None'
+    print '   -a*     features to add                             None'
+    print '   -c*     features to change                          None'
+    print '   -r*     features to remove                          None'
+    print '   -g      the grid of features                        None'
+    print '\033[92m   The keys with the * indicate that the string can be formatted according to the grid.'
+    print '   For example, I could pass the following:'
+    print '         -arg "{\'n_estimators\' : <<N>>}" -g "{\'N\' : [5, 50, 500]}"'
+    print '   which would mean that the model would be ran 3 times, with n_estimators replaced each time.\033[0m' 
 
 if __name__ == '__main__':
     main()
