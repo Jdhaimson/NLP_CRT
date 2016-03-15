74481de0f678318a841067bc2cee4171ef78747e
Josh Haimson <jdhaimson@gmail.com>
2016-03-15 16:31:46 -0400

    Add results viewer

---
+++ view_results.py
@@ -0,0 +1,13 @@
+import argparse
+import pandas as pd
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("id")
+    args = parser.parse_args()
+
+    result_id = int(args.id)
+
+    results = pd.read_csv('/home/ubuntu/experiments/results.csv')
+    print results[results['Id'] == result_id]
