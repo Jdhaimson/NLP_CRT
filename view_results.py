51d3f30b3d57222a66e8d71894e8bd2c82f7303c
Josh Haimson <jdhaimson@gmail.com>
2016-03-16 13:15:58 -0400

    Make view results prettier

--- view_results.py
+++ view_results.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     result_id = int(args.id)
 
     results = pd.read_csv('/home/ubuntu/experiments/results.csv')
-    print results[results['Id'] == result_id]
+    print results[results['Id'] == result_id].iloc[0]
